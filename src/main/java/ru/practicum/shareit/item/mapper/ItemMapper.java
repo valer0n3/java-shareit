538b408f47dc187ff5d380ceaa6f17cf5ebf642a
valer0n3 <vovave@gmail.com>
2023-01-16 13:11:17 +0300

    feat: User and Item mappers were created.

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.item.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@Component
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.isAvailable())
+                .request(item.getRequest()).build();
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/user/dto/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit.user.dto.mapper;
+
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.dto.UserDto;
+
+public class UserMapper {
+    public static UserDto toItemDto(User user) {
+        return UserDto.builder().build();
+    }
+}
