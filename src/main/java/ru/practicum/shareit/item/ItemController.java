1698a96f52bac0f88d9f73796ff666f797ce2a15
valer0n3 <vovave@gmail.com>
2023-01-17 11:27:04 +0300

    feat: ItemControler's methods were implemented.

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +19,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+    @PostMapping
+    public ItemDto addNewItem(@RequestBody ItemDto itemDto, @RequestHeader("X-Sharer-User-Id") int userID) {
+        return null;
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@RequestBody ItemDto itemDto,
+                              @RequestHeader("X-Sharer-User-Id") int userId,
+                              @PathVariable int itemId) {
+        return null;
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(@PathVariable int itemId) {
+        return null;
+    }
+
+    @GetMapping
+    public List<ItemDto> getAllItemsForOwner(@RequestHeader("X-Sharer-User-Id") int userId) {
+        return null;
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItem(@RequestParam String text) {
+        return null;
+    }
 }
