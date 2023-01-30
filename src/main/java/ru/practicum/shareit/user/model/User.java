45eb869a79eaf11513c47c0a8a3ea83f59f0ed61
valer0n3 <vovave@gmail.com>
2023-01-30 18:06:34 +0300

    feat: USer calss was annotated for JPA

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,16 +3,29 @@ package ru.practicum.shareit.user.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id")
     private int id;
-    @NotBlank
+    @Column(name = "name")
     private String name;
-    @NotBlank
-    @Email
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    public User() {
+
+    }
 }
+
